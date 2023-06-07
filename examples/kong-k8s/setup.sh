4955f68f112a5c89091bfaeba95494acae6f7a7a
sonickenbaker <sonickenbaker@gmail.com>
2023-06-07 15:09:49 +0200

    Cleanup

--- examples/kong-k8s/setup.sh
+++ examples/kong-k8s/setup.sh
@@ -48,25 +48,15 @@ kubectl create -n "$NAMESPACE" secret \
   --from-literal=algorithm=RS256 \
   --from-file=rsa_public_key="/home/ubuntu/jwtRS256.key.pub"
 
-# kind only start
-#kubectl patch service -n "$NAMESPACE" kong-proxy-kong-proxy -p '{"spec":{"type":"NodePort"}}'
-#kubectl patch -n "$NAMESPACE" deployment kong-proxy-kong --patch-file /k8s/examples/kong-k8s/deployment-patch.yaml
-# kind only end
-
 kubectl apply -n "$NAMESPACE" -f /k8s/examples/kong-k8s/ingress.yaml
 kubectl apply -f '/k8s/examples/kong-k8s/echo/*-stage.yml'
 
-## Ingress controller and proxy in "prod" namespace
+## Ingress controller and proxy in "prod" namespace - TO BE FIXED
 #NAMESPACE="prod"
 #kubectl create namespace "$NAMESPACE"
 #helm install kong-proxy kong/kong --version "$HELM_CHART_VERSION" --set "ingressController.watchNamespaces={prod}" -f "$HELM_VALUES" -n "$NAMESPACE"
 #kubectl apply -n "$NAMESPACE" -f echo-deployment.yaml
 #kubectl apply -n "$NAMESPACE" -f echo-service.yaml
-#
-## kind only start
-#kubectl patch service -n "$NAMESPACE" kong-proxy-kong-proxy -p '{"spec":{"type":"NodePort"}}'
-#kubectl patch -n "$NAMESPACE" deployment kong-proxy-kong --patch-file /k8s/examples/kong-k8s/deployment-patch.yaml
-## kind only end
 #
 #kubectl apply -n "$NAMESPACE" -f /k8s/examples/kong-k8s/ingress-class.yaml
 #kubectl apply -n "$NAMESPACE" -f /k8s/examples/kong-k8s/ingress.yaml
